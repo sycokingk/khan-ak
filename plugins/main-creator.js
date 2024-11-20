edf228b992973840888516e55618dfc640ea6688
Syco King <89406307+sycokingk@users.noreply.github.com>
2024-11-20 18:34:47 +0500

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,19 +1,18 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
   let vcard = `BEGIN:VCARD
 VERSION:3.0
-N:;Qasim;;;
-FN:Qasim Ali
-ORG:GlobalTechInfo
+N:;Afzaal Khan;;;
+ORG:HackerTechInfo
 TITLE:Owner
-item1.TEL;waid=923444844060:923444844060
+item1.TEL;waid=923556159234:923556159234
 item1.X-ABLabel:Owner
 X-WA-BIZ-DESCRIPTION:Developer of the Bot
-X-WA-BIZ-NAME:Qasim Ali
+X-WA-BIZ-NAME:Afzaal Khan
 END:VCARD`;
 
   await conn.sendMessage(m.chat, {
     contacts: {
-      displayName: 'Qasim Ali',
+      displayName: 'Afzaal Khan',
       contacts: [{ vcard }]
     }
   }, { quoted: m });
