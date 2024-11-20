8e44468fa436ab538a6fb81136855e8e2c6e535f
Syco King <89406307+sycokingk@users.noreply.github.com>
2024-11-20 16:19:43 +0500

    Update Alive.js

--- plugins/Alive.js
+++ plugins/Alive.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   // Sound
   let name = m.pushName || conn.getName(m.sender);
-  let img = 'https://i.imgur.com/s0SqG3g.jpeg';
+  let img = 'https://imgur.com/BNYrqCr';
   let con = {
     key: {
       fromMe: false,
@@ -17,14 +17,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   };
 
   let messageContent = {
-    text: '𝗨𝗟𝗧𝗥𝗔-𝗠𝗗 𝗜𝗦 𝗥𝗨𝗡𝗡𝗜𝗡𝗚', // Text content in case a message body is needed
+    text: 'AK-Bot is running', // Text content in case a message body is needed
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
-        title: '© 𝙶𝚕𝚘𝚋𝚊𝚕𝚃𝚎𝚌𝚑𝙸𝚗𝚏𝚘',
-        body: '© GlobalTechInfo',
+        title: 'HackerTechInfo',
+        body: 'HackerTechInfo',
         thumbnailUrl: img,
-        sourceUrl: 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07',
+        sourceUrl: 'https://whatsapp.com/channel/0029VaHJSUeC6Zvq89X23w2k',
         mediaType: 1,
         renderLargerThumbnail: true,
       },
